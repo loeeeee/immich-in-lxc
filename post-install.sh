d035af05ba018183cddeea2ef20a7d44eb6eb9d5
loeeeee <95266635+loeeeee@users.noreply.github.com>
2024-08-06 11:06:29 +0800

    add post install scripts

---
+++ post-install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -xeuo pipefail # Make my life easier
+
+# -------------------
+# Copy service file
+# -------------------
+SCRIPT_DIR=$PWD
+
+copy_service_files () {
+    cp immich-microservices.service /etc/systemd/system/
+    cp immich-ml.service /etc/systemd/system/
+    cp immich-web.service /etc/systemd/system/
+}
+
+copy_service_files
+
+# -------------------
+# Create log directory
+# -------------------
+
+create_log_directory () {
+    mkdir -p /var/log/immich
+}
+
+create_log_directory
+
+echo "Done!"
