cf9234836ba1448572ceb0dbba977876f3063f36
loeeeee <95266635+loeeeee@users.noreply.github.com>
2024-09-04 22:56:51 +0800

    change permission

--- build_base_images.sh
+++ build_base_images.sh
@@ -7,4 +7,4 @@
 sed -i 's/build-lock.json/base-images\/server\/bin\/build-lock.json/g' base-images/server/bin/build-imagemagick.sh 
 sed -i '/cd .. && rm -rf ImageMagick/d' base-images/server/bin/build-imagemagick.sh
 
-./base-images/server/bin/build-imagemagick.sh 
+./base-images/server/bin/build-imagemagick.sh
