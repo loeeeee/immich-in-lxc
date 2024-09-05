8af521ef6305bda2c420a22b073231f51eec9fd9
loeeeee <95266635+loeeeee@users.noreply.github.com>
2024-09-05 23:04:52 +0800

    build script

--- pre-install.sh
+++ pre-install.sh
@@ -11,26 +11,33 @@ set -xeuo pipefail # Make people's life easier
 SCRIPT_DIR=$PWD
 REPO_URL="https://github.com/immich-app/base-images"
 BASE_IMG_REPO_DIR=$SCRIPT_DIR/base-images
+SOURCE_DIR=$SCRIPT_DIR/image-source
+
+# -------------------
+# Git clone function
+# -------------------
+
+function git_clone () {
+    # $1 is repo URL
+    # $2 is clone target folder
+    # $3 is branch name
+    if [ ! -d "$2" ]; then
+        git clone "$1" "$2"
+    fi
+    cd $2
+    # REMOVE all the change one made to source repo, which is sth not supposed to happen
+    git reset --hard $3
+    # In case one is not on the branch
+    git checkout $3
+    # Get updates
+    git pull
+}
 
 # -------------------
 # Clone the base images repo
 # -------------------
 
-clone_the_base_images_repo () {
-    if [ ! -d "$BASE_IMG_REPO_DIR" ]; then
-        git clone "$REPO_URL" "$BASE_IMG_REPO_DIR"
-    fi
-
-    cd $BASE_IMG_REPO_DIR
-    # REMOVE all the change one made to source repo, which is sth not supposed to happen
-    git reset --hard main
-    # In case one is not on the branch
-    git checkout main
-    # Get updates
-    git pull
-}
-
-clone_the_base_images_repo
+git_clone $REPO_URL $BASE_IMG_REPO_DIR main
 
 # -------------------
 # Change build-lock permission
@@ -44,18 +51,40 @@ change_permission () {
 change_permission
 
 # -------------------
-# Build base images
+# Setup folders
 # -------------------
 
-build_base_images () {
+setup_folders () {
     cd $SCRIPT_DIR
 
-    # ImageMagick
-
-    sed -i 's/build-lock.json/base-images\/server\/bin\/build-lock.json/g' ${BASE_IMG_REPO_DIR}/server/bin/build-imagemagick.sh 
-    sed -i '/cd .. && rm -rf ImageMagick/d' ${BASE_IMG_REPO_DIR}/server/bin/build-imagemagick.sh
-
-    exec $BASE_IMG_REPO_DIR/server/bin/build-imagemagick.sh
+    if [ ! -d "$SOURCE_DIR" ]; then
+        mkdir $SOURCE_DIR
+    fi
 }
 
-build_base_images
+setup_folders
+
+# -------------------
+# Build image magick
+# -------------------
+
+build_image_magick () {
+    cd $SCRIPT_DIR
+
+    SOURCE=$SOURCE_DIR/image-magick
+
+    set -e
+    : "${IMAGEMAGICK_REVISION:=$(jq -cr '.sources[] | select(.name == "imagemagick").revision' build-lock.json)}"
+    set +e
+
+    git_clone https://github.com/ImageMagick/ImageMagick.git $SOURCE $IMAGEMAGICK_REVISION
+
+    cd $SOURCE
+
+    ./configure --with-modules
+    make -j$(nproc)
+    make install
+    ldconfig /usr/local/lib
+}
+
+build_image_magick
