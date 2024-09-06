af4ef02cbb7f6078712503a1a363114d6677a4fb
loeeeee <95266635+loeeeee@users.noreply.github.com>
2024-09-07 00:04:01 +0800

    Revert "implement isFIX"
    This reverts commit aebd764074b399c17ada97b82bc199ae9b5fc878.

--- install.sh
+++ install.sh
@@ -243,10 +243,7 @@ install_immich_machine_learning () {
     fi
 
     # Work around for bad poetry config
-    if [ $isFIX = true ]; then
-        # A goofy fix
-        pip install "numpy<2" -i $PROXY_POETRY
-    fi
+    pip install "numpy<2" -i $PROXY_POETRY
     )
     
     # Copy results
