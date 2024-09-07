cf426afc907afabf1c5a9792f95683b70806ca96
loeeeee <95266635+loeeeee@users.noreply.github.com>
2024-09-08 01:48:48 +0800

    update package installation

--- dep-debian.sh
+++ dep-debian.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-# TODO
+set -xeuo pipefail # Make my life easier
+
+# It turned out that Debian and Ubuntu use the same packages
+
+exec ./dep-ubuntu.sh
--- dep-ubuntu.sh
+++ dep-ubuntu.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -xeuo pipefail # Make my life easier
+
 # Install Dependency for Ubuntu 24.04 LTS
 
 ## From immich/base-image
