f090d8a1f7479f4891d55afd94cb70b213a2f18d
loeeeee <95266635+loeeeee@users.noreply.github.com>
2024-10-02 15:44:26 +0800

    avoid prompting when creating user

--- full-auto.sh
+++ full-auto.sh
@@ -100,7 +100,7 @@ apt install -y git
 
 # Immich user
 
-adduser --shell /bin/bash --disabled-password immich
+adduser --shell /bin/bash --disabled-password immich --comment "Immich Mich"
 
 # Locale
 
