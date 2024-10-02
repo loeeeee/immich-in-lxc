e63f21d242d2ce3d5e3f7bb70db29c76d1de537e
loeeeee <95266635+loeeeee@users.noreply.github.com>
2024-10-03 00:27:09 +0800

    install nvm properly

--- full-auto.sh
+++ full-auto.sh
@@ -123,14 +123,15 @@ fi
 
 # NPM
 
-sudo -u immich -s -n -E -- "cd /home/immich ; curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash ; nvm install 20"
+su immich -c sh -c "cd /home/immich && curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash"
+sudo -u immich -s -n -E -- "nvm install 20"
 
 # Immich
 
 ## Generate config
-sudo -u immich -s -n -E -- "cd /home/immich/immich-in-lxc ; ./install.sh"
+sudo -u immich -s -n -E -- "cd /home/immich/immich-in-lxc && ./install.sh"
 ## Install
-sudo -u immich -s -n -E -- "cd /home/immich/immich-in-lxc ; ./install.sh"
+sudo -u immich -s -n -E -- "cd /home/immich/immich-in-lxc && ./install.sh"
 
 # Post-install
 
