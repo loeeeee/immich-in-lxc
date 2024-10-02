c95b12a21e4c71d10bcc40bdd0ae2d5b25dbc1de
loeeeee <95266635+loeeeee@users.noreply.github.com>
2024-10-02 14:25:45 +0800

    fix getopts

--- full-auto.sh
+++ full-auto.sh
@@ -19,7 +19,6 @@ set -xeuo pipefail # Make people's life easier
 while getopts "R" opt; do
     case $opt in
         R)
-        echo "-a was triggered, Parameter: $OPTARG" >&2
         apt purge -y postgresql-17 postgresql-17-pgvector
         apt purge -y jellyfin-ffmpeg6
         rm /usr/bin/ffmpeg /usr/bin/ffprobe
