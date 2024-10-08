38f5a46050c1cb19ae5b4e3b89c7c09366137b00
loeeeee <95266635+loeeeee@users.noreply.github.com>
2024-10-08 16:38:28 +0800

    update build procedure

--- pre-install.sh
+++ pre-install.sh
@@ -27,8 +27,10 @@ function git_clone () {
         git clone "$1" "$2"
     fi
     cd $2
+    # Get updates
+    git fetch
     # REMOVE all the change one made to source repo, which is sth not supposed to happen
-    git reset --hard $3
+    git reset --hard HEAD
     # In case one is not on the branch
     git checkout $3
     # Get updates
@@ -65,6 +67,56 @@ setup_folders () {
 }
 
 setup_folders
+
+# -------------------
+# Build libjxl
+# -------------------
+
+build_libjxl () {
+    cd $SCRIPT_DIR
+
+    SOURCE=$SOURCE_DIR/libjxl
+
+    set -e
+    : "${LIBJXL_REVISION:=$(jq -cr '.sources[] | select(.name == "libjxl").revision' $BASE_IMG_REPO_DIR/server/bin/build-lock.json)}"
+    set +e
+
+    git_clone https://github.com/libjxl/libjxl.git $SOURCE $LIBJXL_REVISION
+
+    cd $SOURCE
+
+    git submodule update --init --recursive --depth 1 --recommend-shallow
+
+    
+    mkdir build
+    cd build
+    cmake \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DBUILD_TESTING=OFF \
+    -DJPEGXL_ENABLE_DOXYGEN=OFF \
+    -DJPEGXL_ENABLE_MANPAGES=OFF \
+    -DJPEGXL_ENABLE_PLUGIN_GIMP210=OFF \
+    -DJPEGXL_ENABLE_BENCHMARK=OFF \
+    -DJPEGXL_ENABLE_EXAMPLES=OFF \
+    -DJPEGXL_FORCE_SYSTEM_BROTLI=ON \
+    -DJPEGXL_FORCE_SYSTEM_HWY=ON \
+    -DJPEGXL_ENABLE_JPEGLI=ON \
+    -DJPEGXL_ENABLE_JPEGLI_LIBJPEG=ON \
+    -DJPEGXL_INSTALL_JPEGLI_LIBJPEG=ON \
+    -DJPEGXL_ENABLE_AVX512=ON \
+    -DJPEGXL_ENABLE_AVX512_ZEN4=ON \
+    -DJPEGXL_ENABLE_PLUGINS=ON \
+    ..
+    cmake --build . -- -j$(nproc)
+    cmake --install .
+
+    ldconfig /usr/local/lib
+
+    # Clean up builds
+    make clean
+}
+
+build_libjxl
 
 # -------------------
 # Build libheif
@@ -187,3 +239,6 @@ build_libvips () {
 }
 
 build_libvips
+
+# Dockerfile 109
+dpkg -r --force-depends libjpeg62-turbo
