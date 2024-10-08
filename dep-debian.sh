7d4beb4a7c1d100afbcbd70ab90922bbfbbbee8b
loeeeee <95266635+loeeeee@users.noreply.github.com>
2024-10-08 16:38:16 +0800

    update build dependency

--- dep-debian.sh
+++ dep-debian.sh
@@ -6,8 +6,21 @@ set -xeuo pipefail # Make my life easier
 
 ./dep-common.sh
 
-apt install -t testing -y \
-        libio-compress-brotli-perl \
-        libwebp-dev \
+# Dockerfile 35
+apt install -t testing --no-install-recommends -y \
         libdav1d-dev \
-        libjxl-dev
+        libhwy-dev \
+        libwebp-dev \
+        libio-compress-brotli-perl
+
+## Dockerfile 92
+apt install -t testing --no-install-recommends -y \
+        libio-compress-brotli-perl \
+        libwebp7 \
+        libwebpdemux2 \
+        libwebpmux3 \
+        libhwy1t64
+
+## Dockerfile 104
+apt install -t testing --no-install-recommends -y \
+        intel-media-va-driver-non-free
