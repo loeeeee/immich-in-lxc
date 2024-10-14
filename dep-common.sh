d8917a2fa7bbe8090fed04369a5d5c47ad9df946
loeeeee <95266635+loeeeee@users.noreply.github.com>
2024-10-14 12:59:07 +0800

    skip intel graphic diver

--- dep-common.sh
+++ dep-common.sh
@@ -66,14 +66,15 @@ apt install --no-install-recommends -y\
         ocl-icd-libopencl1
 
 # Install Intel things
-if [ ! -d ".intel-things-success-0" ]; then
-    mkdir /tmp/immich-preinstall
-    cd /tmp/immich-preinstall
-    wget https://github.com/intel/intel-graphics-compiler/releases/download/igc-1.0.17193.4/intel-igc-core_1.0.17193.4_amd64.deb
-    wget https://github.com/intel/intel-graphics-compiler/releases/download/igc-1.0.17193.4/intel-igc-opencl_1.0.17193.4_amd64.deb
-    wget https://github.com/intel/compute-runtime/releases/download/24.26.30049.6/intel-opencl-icd_24.26.30049.6_amd64.deb
-    wget https://github.com/intel/compute-runtime/releases/download/24.26.30049.6/libigdgmm12_22.3.20_amd64.deb
-    dpkg -i *.deb
-    touch .intel-things-success-0
-    rm -r /tmp/immich-preinstall
-fi
+# Skip it
+# if [ ! -d ".intel-things-success-0" ]; then
+#     mkdir /tmp/immich-preinstall
+#     cd /tmp/immich-preinstall
+#     wget https://github.com/intel/intel-graphics-compiler/releases/download/igc-1.0.17193.4/intel-igc-core_1.0.17193.4_amd64.deb
+#     wget https://github.com/intel/intel-graphics-compiler/releases/download/igc-1.0.17193.4/intel-igc-opencl_1.0.17193.4_amd64.deb
+#     wget https://github.com/intel/compute-runtime/releases/download/24.26.30049.6/intel-opencl-icd_24.26.30049.6_amd64.deb
+#     wget https://github.com/intel/compute-runtime/releases/download/24.26.30049.6/libigdgmm12_22.3.20_amd64.deb
+#     dpkg -i *.deb
+#     touch .intel-things-success-0
+#     rm -r /tmp/immich-preinstall
+# fi
