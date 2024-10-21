18cf7b8c393484039945e3e240f0f41794893120
loeeeee <95266635+loeeeee@users.noreply.github.com>
2024-10-21 16:00:43 +0800

    use the libjpeg in libjxl

--- dep-ubuntu.sh
+++ dep-ubuntu.sh
@@ -8,10 +8,10 @@ set -xeuo pipefail # Make my life easier
 
 # libjpeg62-turbo-dev
 ## Fix my previous mistake
-apt purge -y \
-        libjpeg62-dev
-apt install --no-install-recommends -y \
-        libjpeg-turbo8-dev
+# apt purge -y \
+#         libjpeg62-dev
+# apt install --no-install-recommends -y \
+#         libjpeg-turbo8-dev
 ## libjpeg-turbo is faster than libjpeg
 
 # Dockerfile 35
