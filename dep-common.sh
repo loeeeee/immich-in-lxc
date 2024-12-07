1a13158923619162d5be1c3a32098f21e6e9bc74
loeeeee <95266635+loeeeee@users.noreply.github.com>
2024-12-07 14:39:49 +0800

    add dedicate intel/OpenVINO dependency

--- dep-common.sh
+++ dep-common.sh
@@ -72,17 +72,3 @@ apt install --no-install-recommends -y\
         wget \
         zlib1g \
         ocl-icd-libopencl1
-
-# Install Intel things
-# Skip it
-# if [ ! -d ".intel-things-success-0" ]; then
-#     mkdir /tmp/immich-preinstall
-#     cd /tmp/immich-preinstall
-#     wget https://github.com/intel/intel-graphics-compiler/releases/download/igc-1.0.17384.11/intel-igc-core_1.0.17384.11_amd64.deb
-#     wget https://github.com/intel/intel-graphics-compiler/releases/download/igc-1.0.17384.11/intel-igc-opencl_1.0.17384.11_amd64.deb
-#     wget https://github.com/intel/compute-runtime/releases/download/24.31.30508.7/intel-opencl-icd_24.31.30508.7_amd64.deb
-#     wget https://github.com/intel/compute-runtime/releases/download/24.31.30508.7/libigdgmm12_22.4.1_amd64.deb 
-#     dpkg -i *.deb
-#     touch .intel-things-success-0
-#     rm -r /tmp/immich-preinstall
-# fi
---
+++ dep-intel.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -xeuo pipefail # Make my life easier
+
+# :warning: This script is only needed for those would like to use Intel/OpenVINO ONNX execution provider.
+
+# Variables
+TEMP_DIR=/tmp/immich-intel
+
+mkdir $TEMP_DIR
+cd $TEMP_DIR
+
+# Copied from immich/machine-learning/Dockerfile line 41
+apt-get install --no-install-recommends -yqq ocl-icd-libopencl1 wget
+wget https://github.com/intel/intel-graphics-compiler/releases/download/igc-1.0.17384.11/intel-igc-core_1.0.17384.11_amd64.deb
+wget https://github.com/intel/intel-graphics-compiler/releases/download/igc-1.0.17384.11/intel-igc-opencl_1.0.17384.11_amd64.deb
+wget https://github.com/intel/compute-runtime/releases/download/24.31.30508.7/intel-opencl-icd_24.31.30508.7_amd64.deb
+wget https://github.com/intel/compute-runtime/releases/download/24.31.30508.7/libigdgmm12_22.4.1_amd64.deb
+dpkg -i *.deb
+
+# Clean up
+rm -r $TEMP_DIR
+
+echo "All Good"
