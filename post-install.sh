83e1cb2efa741c8ce498c922f762c79be6b07d2d
Xinyang Li <soybeanyoung@gmail.com>
2025-04-01 09:10:02 -0400

    Fix post-install
    1. Environment variables were not loaded
    2. & symbols were not escaped in the sed command

--- post-install.sh
+++ post-install.sh
@@ -20,6 +20,8 @@ load_environment_variables () {
     set +a
 }
 
+load_environment_variables
+
 copy_service_files () {
     # Remove deprecated service
     rm -f /etc/systemd/system/immich-microservices.service
@@ -28,7 +30,7 @@ copy_service_files () {
     MINOR_VERSION=$(echo $REPO_TAG | cut -d'.' -f2)
     if [ $MINOR_VERSION -gt 130 ]; then
         sed -i -e '0,/^WorkingDirectory/ s,^WorkingDirectory.*,WorkingDirectory=/home/immich/app/machine-learning,' \
-               -e "0,/^ExecStart/ s,^ExecStart.*,ExecStart=/bin/bash -c 'source /home/immich/app/machine-learning/venv/bin/activate && python -m immich_ml'," \
+               -e "0,/^ExecStart/ s,^ExecStart.*,ExecStart=/bin/bash -c 'source /home/immich/app/machine-learning/venv/bin/activate \&\& python -m immich_ml'," \
                /etc/systemd/system/immich-ml.service
     fi
     cp immich-web.service /etc/systemd/system/
